2385f51e11d8535b02f974f49387eeaf03d41541
DESKTOP-8B4QBQ3\Windows <neal.robben@gmail.com>
2023-11-08 22:23:37 +0100

    Added override for lifecycle methods

---
+++ Pages/Lifecycle.razor.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorLifeCycle.Pages;
+
+public partial class Lifecycle
+{
+    public override Task SetParametersAsync(ParameterView parameters)
+    {
+        Console.WriteLine("{0}()", nameof(SetParametersAsync));
+
+        return base.SetParametersAsync(parameters);
+    }
+
+    protected override void OnInitialized()
+    {
+        Console.WriteLine("{0}()", nameof(OnInitialized));
+
+        base.OnInitialized();
+    }
+
+    protected override Task OnInitializedAsync()
+    {
+        Console.WriteLine("{0}()", nameof(OnInitializedAsync));
+
+        return base.OnInitializedAsync();
+    }
+
+    protected override void OnParametersSet()
+    {
+        Console.WriteLine("{0}()", nameof(OnParametersSet));
+
+        base.OnParametersSet();
+    }
+
+    protected override Task OnParametersSetAsync()
+    {
+        Console.WriteLine("{0}()", nameof(OnParametersSetAsync));
+
+        return base.OnParametersSetAsync();
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        Console.WriteLine("{0}({1}: {2})",
+            nameof(OnAfterRender),
+            nameof(firstRender),
+            firstRender.ToString().ToLower());
+
+        base.OnAfterRender(firstRender);
+    }
+
+    protected override Task OnAfterRenderAsync(bool firstRender)
+    {
+        Console.WriteLine("{0}({1}: {2})",
+            nameof(OnAfterRenderAsync),
+            nameof(firstRender),
+            firstRender.ToString().ToLower());
+
+        return base.OnAfterRenderAsync(firstRender);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # BlazorLifecycle
+
+
+https://jonathancrozier.com/blog/understanding-the-blazor-component-lifecycle
+
+https://blazor-university.com/components/component-lifecycles/
+
+https://learn.microsoft.com/en-us/aspnet/core/blazor/components/lifecycle?view=aspnetcore-7.0
